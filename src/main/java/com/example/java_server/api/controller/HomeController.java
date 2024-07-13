dff9e15ead1864e8f7a7116c6ee9d3b3aaa1ede6
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-13 10:32:43 +0530

    feat: java server

--- src/main/java/com/example/java_server/api/controller/HomeController.java
+++ src/main/java/com/example/java_server/api/controller/HomeController.java
@@ -2,3 +2,16 @@
 // import com.example.springapi.service.UserService;
 
 
+package com.example.java_server.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public ResponseEntity<String> Home() {
+        return ResponseEntity.ok("Hello From Spring-boot Server");
+    }
+}

21ac982462f1c26bb53cffc05b71845ffa200680
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-20 21:15:43 +0530

    feat: added project files

--- src/main/java/com/example/springboot_server/SpringbootServerApplication.java
+++ src/main/java/com/example/springboot_server/SpringbootServerApplication.java
@@ -39,52 +39,52 @@ public class SpringbootServerApplication {
 				throw new RuntimeException("Failed to load .env file", e);
 			}
 		}
-		
+
 	}
 
-		@ConfigurationProperties(prefix = "spring.datasource")
-		@Bean
-		public DatabaseConfig databaseConfig(Environment environment) {
-			return new DatabaseConfig(
-					environment.getProperty("username"),
-					environment.getProperty("password"),
-					environment.getProperty("url"));
-		}
+	@ConfigurationProperties(prefix = "spring.datasource")
+	@Bean
+	public DatabaseConfig databaseConfig(Environment environment) {
+		return new DatabaseConfig(
+				environment.getProperty("username"),
+				environment.getProperty("password"),
+				environment.getProperty("url"));
+	}
+}
+
+class DatabaseConfig {
+	private String username;
+	private String password;
+	private String url;
+
+	public DatabaseConfig(String username, String password, String url) {
+		this.username = username;
+		this.password = password;
+		this.url = url;
 	}
 
-	class DatabaseConfig {
-		private String username;
-		private String password;
-		private String url;
+	public String getUsername() {
+		return username;
+	}
 
-		public DatabaseConfig(String username, String password, String url) {
-			this.username = username;
-			this.password = password;
-			this.url = url;
-		}
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-		public String getUsername() {
-			return username;
-		}
+	public String getPassword() {
+		return password;
+	}
 
-		public void setUsername(String username) {
-			this.username = username;
-		}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-		public String getPassword() {
-			return password;
-		}
+	public String getUrl() {
+		return url;
+	}
 
-		public void setPassword(String password) {
-			this.password = password;
-		}
-
-		public String getUrl() {
-			return url;
-		}
-
-		public void setUrl(String url) {
-			this.url = url;
-		}
+	public void setUrl(String url) {
+		this.url = url;
+	}
 
 }

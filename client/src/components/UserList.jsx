f466d730267abbf86cc58af962d582a690dbc282
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-23 13:03:05 +0530

    feat: added responsiveness

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -31,46 +31,51 @@ const UserList = () => {
 	}
 
 	return (
-		<div className="p-4">
-			<h1 className="text-2xl font-bold mb-4">User List</h1>
-			<table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
-				<thead>
-					<tr>
-						<th className="py-2 px-4 border-b">ID</th>
-						<th className="py-2 px-4 border-b">Name</th>
-					</tr>
-				</thead>
-				<tbody>
-					{users.map((user) => (
-						<tr key={user.id}>
-							<td className="py-2 px-4 border-b">{user.id}</td>
-							<td className="py-2 px-4 border-b">{user.name}</td>
+		<div className="p-4 max-w-screen-lg mx-auto">
+			<h1 className="text-2xl font-bold mb-4 text-center">User List</h1>
+			{/* Form for Adding New User */}
+
+			{/* Table to Display Users */}
+			<div className="overflow-x-auto">
+				<table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
+					<thead>
+						<tr>
+							<th className="py-2 px-4 border-b text-left">ID</th>
+							<th className="py-2 px-4 border-b text-left">Name</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
-			<div className="flex gap-6 my-5">
-				<div className="mb-4">
-					<input
-						type="text"
-						value={newUserName}
-						onChange={(e) => setNewUserName(e.target.value)}
-						placeholder="Enter user name"
-						className="mr-2 px-4 py-2 border border-gray-300 rounded"
-					/>
+					</thead>
+					<tbody>
+						{users.map((user) => (
+							<tr key={user.id}>
+								<td className="py-2 px-4 border-b">{user.id}</td>
+								<td className="py-2 px-4 border-b">{user.name}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+				<div className="flex flex-col sm:flex-row sm:items-center gap-4 mb-4 py-4">
+					<div className="flex flex-col sm:flex-row items-center gap-2">
+						<input
+							type="text"
+							value={newUserName}
+							onChange={(e) => setNewUserName(e.target.value)}
+							placeholder="Enter user name"
+							className="px-4 py-2 border border-gray-300 rounded w-full sm:w-auto"
+						/>
+						<button
+							onClick={createUser}
+							className="mt-2 sm:mt-0 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 w-full sm:w-auto"
+						>
+							Create New User
+						</button>
+					</div>
 					<button
-						onClick={createUser}
-						className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+						onClick={fetchUsers}
+						className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full sm:w-auto"
 					>
-						Create New User
+						Get All Users
 					</button>
 				</div>
-				<button
-					onClick={fetchUsers}
-					className="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-				>
-					Get All Users
-				</button>
 			</div>
 		</div>
 	)
